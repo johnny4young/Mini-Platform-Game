dc0fe77805089bb807bc4fba0fdb0e39fb9967b9
johnny4young <asesordeprogramacion@gmail.com>
2020-07-29 21:56:26 -0500

    feat: smooth character movement

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -11,24 +11,33 @@ var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
+	var friction = false
 	
 	if Input.is_action_pressed("ui_right"):
 		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -ACCELERATION
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2)
+		friction = true
 		$Sprite.play("Idle")
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
 	else:
-		$Sprite.play("Jump")
+		if motion.y < 0:
+			$Sprite.play("Jump")
+		else:
+			$Sprite.play("Fall")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 	motion = move_and_slide(motion, UP)
 	
 	pass
