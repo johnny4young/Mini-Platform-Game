6af716b362ac01a4329797e029a0462b0b54aea0
johnny4young <asesordeprogramacion@gmail.com>
2020-07-29 21:33:23 -0500

    feat: added Tiles and animation player

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,7 +2,8 @@ extends KinematicBody2D
 
 const UP = Vector2(0,-1)
 const GRAVITY = 20
-const SPEED = 200
+const ACCELERATION = 50
+const MAX_SPEED = 200
 const JUMP_HEIGHT = -600
 
 var motion = Vector2()
@@ -12,16 +13,22 @@ func _physics_process(delta):
 	motion.y += GRAVITY
 	
 	if Input.is_action_pressed("ui_right"):
-		motion.x = SPEED
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$Sprite.flip_h = false
+		$Sprite.play("Run")
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -SPEED
+		motion.x = -ACCELERATION
+		$Sprite.flip_h = true
+		$Sprite.play("Run")
 	else:
 		motion.x = 0
+		$Sprite.play("Idle")
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
-			
+	else:
+		$Sprite.play("Jump")
 	motion = move_and_slide(motion, UP)
 	
 	pass
